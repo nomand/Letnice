4e84c689ec8b5cd2767905c5162ebf111cb8a103
nomand <alexey.botkov@gmail.com>
2018-06-04 22:29:09 +1200

    wip days

--- js/letnice.js
+++ js/letnice.js
@@ -4,81 +4,24 @@ function Year()
 
   let year = new Date().getFullYear();
 
-  const monthNames = [
-    "January", "February", "March",
-    "April", "May", "June",
-    "July", "August", "September",
-    "October", "November", "December"
-  ];
-  
-  const dayNames = [
-    "Sunday", "Monday", "Tuesday", "Wednesday",
-    "Thursday", "Friday", "Saturday" 
-  ]
+  const monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+  const dayNames = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ]
 
   // let leap = new Date(year, 1, 29).getDate() === 29;
   // let days = leap ? 366 : 365;
   // let cell = parseInt((base.getBoundingClientRect().width - 52*2)/52);
 
-  let week = 0;
   let count = 1;
   let month = 0;
 
   let style = "day";
-
-  // make grid
-  // while(week < 52)
-  // {
-  //   let x = parseInt(week * (cell+2));
-  //   let day = 0;
-  //   while(day < 7)
-  //   {
-  //     let y = parseInt(day * (cell+2))
-  //     count++;
-  //     base.innerHTML += `<rect class='day' x='${x}' y='${y}' title='${count}' width='${cell}' height='${cell}' rx="2" ry="2" onclick=""></rect>`;
-  //     day += 1
-  //   }
-  //   week += 1
-  // }
-
-//  let i = 0;
-//   while(month < 12)
-//   {
-//     let day = 0;
-//     let x = 0;
-//     while(day < new Date(year, month).monthDays(month))
-//     {
-//       if(i === 7)
-//       {
-//         week++
-//         i=0
-//       }
-//       day++;
-//       i++
-//       count++;
-//       let y = parseInt(count * (cell+1));
-//       console.log(count + ", M" + month + ", W" + week + ", D" + day);
-//       base.innerHTML += `<rect class='${CheckDay(year, month, day)}' x='${x}' y='${y}' title='${count}' width='${cell}' height='${cell}' rx="2" ry="2" onclick=""></rect>`;
-//     }
-//     month++;
-//   }
-// }
-
-    //start container
-    //add month text
-      //mtwtfss
-        //start comparing date with weekdays
-          //start iterating through days from a match until month is over
-
-
-  //let cell = parseInt((base.getBoundingClientRect().width - 52*2)/52)
-
+  
   while(month < 12)
   {
     base.innerHTML += `
       <div class="month">
       <p class="m" x="" y='${month * 140}' dy="10">${monthNames[new Date(year, month).getMonth()].substr(0,2)}</p>
-      <svg class="graph">
+      <svg class="graph" id="${monthNames[month]}">
       ${dayLabels()}
       ${populateMonth(month)}
       </svg></div>`
@@ -89,36 +32,43 @@ function Year()
   {
     let html = "";
     let y = 0;
-
+    
     for(i = 0; i < 7; i++)
     {
-      y = i * 20;
-      html += `<text class="dayLabel" x="7" y='${y}' dy="10">${dayNames[i].substr(0,1)}</text>`
+      y = i * 14;
+      html += `<text class="dayLabel" x="3" y='${y}' dy="10">${dayNames[i].substr(0,1)}</text>`
     }
     return html;
   }
 
-  function populateMonth(m)
+  function populateMonth(month)
   {
     html = "";
-    monthLength = new Date(year, m, 0).getDate();
+    monthLength = new Date(year, month+1, 0).getDate();
     let day = 0;
-    let x = 14;
+    let x = 0;
     let y = 0;
 
     while(day < monthLength)
     {
-      let i = 0
       x += 14;
+      let weekDay = 0
       
-      while(i < 7)
+      while(weekDay < 7 && day != monthLength)
       {
-        y = i * 14;
+        y = weekDay * 14;
         
-        //is it monday? is it tuesday? etc. instantiate null until day match then start for every day;
-        
-        html += `<rect class="day" x='${x}' y='${y}' title='${day}' width="12px" height="12px" rx="2" ry="2" onclick=""></rect>`
-        i++
+        if(new Date(year, month, day).getDay() != weekDay)
+        {
+          style = "null";
+        }
+        else
+        {
+          style = "day";
+        }
+
+        html += `<rect class='${style}' x='${x}' y='${y}' title='${day+1}' width="12px" height="12px" rx="2" ry="2" onclick=""></rect>`
+        weekDay++
         day++
         count++
       }
@@ -126,33 +76,3 @@ function Year()
     return html;
   }
 }
-
-
-  
-  // while(count <= days)
-  // {
-  //   t = new Date(year, month, count)
-  //   let x = cell;
-  //   let y = parseInt(count * (cell+2));
-    
-  //   console.log(t);
-
-  //   base.innerHTML += `<rect class='${CheckDay(t)}' x='${x}' y='${y}' title='${count}' width='${cell}' height='${cell}' rx="2" ry="2" onclick=""></rect>`;
-  //   count++;
-  // }
-
-  // function CheckDay(day)
-  // {
-  //   switch(day.getDay())
-  //   {
-  //     case 0:
-  //       style = "weekend";
-  //       break;
-  //     case 6:
-  //       style = "weekend";
-  //       break;
-  //     default:
-  //       style = "day";
-  //   }
-  //   return style;
-  // }
