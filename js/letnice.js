92b6ea22d3063d3c4ba129b9b83fb81efddf48d2
nomand <alexey.botkov@gmail.com>
2018-06-05 23:43:26 +1200

    today

--- js/letnice.js
+++ js/letnice.js
@@ -3,16 +3,12 @@ function Year()
   let base = document.getElementById("center");
 
   const monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
-  const dayNames = [ "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" ]
+  const dayNames = [ "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",  "Sunday" ]
 
   let year = new Date().getFullYear();
   let month = 0;
+  let today = new Date(year, new Date().getMonth(), new Date().getDate()-1, 0);
   let style = "day";
-  
-  function doHeader()
-  {
-    return `<div class="header"><p class="y">${year}</p><p class="p">${yearProgress(year) + "%"}</p></div>`;
-  }
 
   while(month < 12)
   {
@@ -26,6 +22,17 @@ function Year()
     month++
   }
 
+  function doHeader()
+  {
+    return `<div class="header"><p class="y">${year}</p><p class="p">${yearProgress(year) + "%"}</p></div>`;
+  }
+
+  function yearProgress(year)
+  {
+    progress = new Date() - new Date(year, 0, 1, 0);
+    return ((progress/31536000000)*100).toFixed(2);
+  }
+
   function doLabels()
   {
     let html = "";
@@ -33,7 +40,7 @@ function Year()
     
     for(i = 0; i < 7; i++)
     {
-      y = (i * 14)+1;
+      y = (i * 14) + 1;
       html += `<text class="dayLabel" x="5" y='${y}' dy="10">${dayNames[i].substr(0,1)}</text>`
     }
     return html;
@@ -46,18 +53,17 @@ function Year()
     let date = 0;
     let x = 0;
     let y = 0;
-    let today = new Date(year, new Date().getMonth(), new Date().getDate(), 0);
-
+    
     while(date < monthLength)
     {
       x += 14;
-      let week = 0
+      let week = 0;
       
       while(week < 7 && date != monthLength)
       {
         y = week * 14 + 2;
-        let day = new Date(year, month, date);
-
+        let day = new Date(year, month, date, 0);
+        
         if(day.getDay() != week)
         {
           style = "null";
@@ -71,22 +77,19 @@ function Year()
         {
           style = "weekend";
         }
+        else if(String(day) == String(today))
+        {
+          style = "today";
+        }
         else
         {
           style = "day";
         }
-
-        html += `<rect class='${style}' x='${x}' y='${y}' title='${dayNames[week] + "_" + (date+1)}' width="12px" height="12px" rx="2" ry="2" onclick=""></rect>`
+        html += `<rect class='${style}' x='${x}' y='${y}' title='${(date+1) == 0 ? "null" : dayNames[week] + " " + (date+1)}' width="12px" height="12px" rx="2" ry="2" onclick=""></rect>`
         week++
         date++
       }
     }
     return html;
-  }
-
-  function yearProgress(year)
-  {
-    progress = new Date() - new Date(year, 0, 1, 0);
-    return ((progress/31536000000)*100).toFixed(2);
   }
 }
